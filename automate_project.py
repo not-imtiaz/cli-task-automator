d771a2e21f6e0f725fdbb432986184565a2b1aa0
not-imtiaz <imtiazyt.ia@gmail.com>
2026-06-29 19:27:51 +0600

    3

--- automate_project.py
+++ automate_project.py
@@ -2,3 +2,12 @@ import argparse
 import os
 import subprocess
 import sys
+
+
+def create_project(project_name):
+    directories = [project_name, os.path.join(
+        project_name, 'src'), os.path.join(project_name, 'assets')]
+    files = {
+        os.path.join(project_name, 'README.md'): f"# {project_name}\nProject initialized automatically.",
+        os.path.join(project_name, '.gitignore'): "__pycache__/\n*.pyc\n.env\n.DS_Store"
+    }
